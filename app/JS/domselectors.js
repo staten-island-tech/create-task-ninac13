aad01d502ce0f15f4a9b2969b1fc27b446bc19dc
ninac13 <144480538+ninac13@users.noreply.github.com>
2024-12-16 14:01:20 -0500

    12/16

--- app/JS/domselectors.js
+++ app/JS/domselectors.js
@@ -1,5 +1,16 @@
 const DOMSelectors = {
-
+    breakfast: document.querySelector("#breakfastBtn"),
+    starter: document.querySelector("#starterBtn"),
+    vegan: document.querySelector("#veganBtn"),
+    vegetarian: document.querySelector("#vegetarianBtn"),
+    side: document.querySelector("#sideBtn"),
+    seafood: document.querySelector("#seafoodBtn"),
+    pasta: document.querySelector("#pastaBtn"),
+    chicken: document.querySelector("#chickenBtn"),
+    lamb: document.querySelector("#lambBtn"),
+    pork: document.querySelector("#porkBtn"),
+    beef: document.querySelector("#beefBtn"),
+    dessert: document.querySelector("#dessertBtn"),
 }
 
 export{DOMSelectors};
--- app/JS/main.js
+++ app/JS/main.js
@@ -1,8 +1,21 @@
 import '../CSS/style.css';
 import { DOMSelectors } from "./domselectors";
 
-const categories = ""
+const URL = "https://www.themealdb.com/api/json/v1/1/filter.php?c="; //use this to add on categories!!
 
-async function fetchData(categories) {
-  
+async function fetchData(URL) {
+  try {
+
+    const response = await fetch(breakfast);
+    if (response.status != 200){
+        throw new Error(response);  
+    }
+    else{
+        const categoriesData = await categories.json();
+        //call function here
+    }
+  } catch (error) {
+    alert ("There is no agent found");
+    console.log ("error");
+  }
 }
